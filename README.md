d7564f93a8690888c334b000dca108d767c2a240
Lori Winston <Lcwinston@gmail.com>
2021-01-25 21:28:18 -0800

    localstorage incrementing

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # EDIT-THIS-README
+
+
+
+Make sure 3 pokemon aren't the same
+
+
+
+
+
+Set Pokemon function
+
+
+render poke img 
+
+
+
+Set pokemon function
+
+-generate random number between 0 and amount of pokemon we have
+
+go through while loop to generate three diff pokemon
---
+++ utils.js
@@ -0,0 +1,16 @@
+import pokemon from './data.js';
+
+export function getRandomPokemon() {
+    const randomIndex = Math.floor(Math.random() * pokemon.length);
+
+    return pokemon[randomIndex];
+}
+
+
+export function findByUnderscoreId(array, id) {
+    for (let item of array) {
+        if (item._id === id) return item;
+    }
+}
+
+
